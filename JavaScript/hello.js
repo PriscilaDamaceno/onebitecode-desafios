345551108479abfa07938ee82cc244dde80b8381
Priscila <pri.damasceno@live.com>
2021-11-18 14:01:15 -0300

    JavaScript desafio 1

---
+++ JavaScript/hello.js
@@ -0,0 +1,9 @@
+alert("Bem-vindo ao programa")
+let nomeMaisVelha = prompt("Insira o nome da pessoa mais velha.")
+let idadeMaisVelha = prompt("Insira a idade da pessoa mais velha.")
+let nomeMaisNova = prompt("Insira o nome da pessoa mais nova.")
+let idadeMaisNova = prompt("Insira a idade da pessoa mais nova.")
+
+let diferencaIdade = idadeMaisVelha - idadeMaisNova
+
+alert("A pessoa mais velha é " + nomeMaisVelha + " com " + idadeMaisVelha + " anos.\nA pessoa mais nova é " + nomeMaisNova + " com " + idadeMaisNova + " anos.\nA diferença de idades é de " + diferencaIdade + " anos.")
